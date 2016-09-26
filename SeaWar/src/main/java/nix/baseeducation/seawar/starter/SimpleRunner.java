47de602d8f6b7766c4aed0fbb3f8a366dca1f7c1
Aleksander Kuschev <aleksandr.kuschev@nixsolutions.com>
2016-09-26 13:24:54 +0300

    Remove runner add SimpleRunner

--- SeaWar/src/main/java/nix/baseeducation/seawar/starter/SimpleRunner.java
+++ SeaWar/src/main/java/nix/baseeducation/seawar/starter/SimpleRunner.java
@@ -1,5 +1,13 @@
-package main.java.nix.baseeducation.seawar.starter;
+package nix.baseeducation.seawar.starter;
+
+import nix.baseeducation.seawar.controller.Game;
+import nix.baseeducation.seawar.controller.PvEConsoleGame;
 
 public class SimpleRunner {
+    
+    public static void main(String[] args) {
+        Game game = new PvEConsoleGame(10);
+        game.startNewGame();
+    }
 
 }
